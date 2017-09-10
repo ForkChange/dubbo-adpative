3fbaeeae28596e415b9b0526131d4cc17782773f
yong.zhao <yong.zhao@weimob.com>
2017-09-10 23:41:46 +0800

    init test case

---
+++ src/test/java/com/alibaba/dubbo/AppTest.java
@@ -0,0 +1,20 @@
+package com.alibaba.dubbo;
+
+import org.junit.Test;
+
+/**
+ * {@link AppTest }
+ *
+ * @author <a href="mailto:zhaoyong_carl@sina.com">Mercy<a/>
+ * @version 1.0.0
+ * @see AppTest
+ * @since 1.0.0 2017-09-10
+ */
+public class AppTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
